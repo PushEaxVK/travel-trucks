04567e1854f037f5545a3b1e292caf158c85b445
PushEax <pusheax.vk@gmail.com>
2025-08-21 00:04:22 +0300

    add navigation to app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import Container from './Container';
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
 
 function AppBar() {
+  const linkStyle =
+    'font-medium text-[16px] leading-[1.5] text-center text-dark';
+  const activeLinkStyle = 'text-red';
+  const setActiveClass = ({ isActive }) => {
+    return clsx(linkStyle, isActive && activeLinkStyle);
+  };
+
   return (
-    <header>
+    <header className="bg-grayExtraLight border-b border-grayLighter">
       <Container>
-        <h1>AppBar</h1>
+        <div className="h-[72px] flex items-center justify-start flex-row gap-[450px]">
+          <a href="/">
+            <svg className="w-[136px] h-[16px]">
+              <use href="/icons.svg#logo"></use>
+            </svg>
+          </a>
+          <nav>
+            <ul className="flex gap-8">
+              <li>
+                <NavLink className={setActiveClass} to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className={setActiveClass} to="/catalog">
+                  Catalog
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </Container>
     </header>
   );
