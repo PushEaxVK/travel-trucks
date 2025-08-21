80336dd4438fea1c546280ec672ba2b4ff775d2f
PushEax <pusheax.vk@gmail.com>
2025-08-21 19:00:56 +0300

    add catalog list with mock data

--- src/components/CatalogList.jsx
+++ src/components/CatalogList.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Button from './Button';
+import CapmerCard from './CapmerCard';
 
 function CatalogList() {
   return (
     <div className="border border-amber-950 w-full flex items-center flex-col gap-10">
-      <div>CatalogList</div>
+      <div className="w-full flex flex-col gap-8">
+        <CapmerCard />
+        <CapmerCard />
+        <CapmerCard />
+        <CapmerCard />
+        <CapmerCard />
+      </div>
       <Button as="button" variant="white" onClick={() => {}} width={145}>
         Load more
       </Button>
