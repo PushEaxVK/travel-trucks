efbe215b171cfb5f579a2031be5aa86022af44e6
PushEax <pusheax.vk@gmail.com>
2025-08-21 19:00:00 +0300

    load sprite in layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppBar from './AppBar';
 import { Outlet } from 'react-router-dom';
+import { SvgSprite } from './SvgSprite';
 
 function Layout() {
   return (
     <>
+      <SvgSprite />
       <AppBar />
       <main className="min-h-screen bg-no-repeat bg-cover w-full">
         <Outlet />
