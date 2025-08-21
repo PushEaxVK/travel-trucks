2b154f7210adef114bce2f33fcfe37161772e4c4
PushEax <pusheax.vk@gmail.com>
2025-08-21 19:00:42 +0300

    add camper card with mock data

---
+++ src/components/CapmerCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from './Button';
+import Icon from './Icon';
+
+function CapmerCard() {
+  return (
+    <div className="p-6 flex flex-row gap-6 border border-grayLight rounded-[20px]">
+      <div className="rounded-[10px] w-[292px] h-[320px] overflow-hidden shrink-0">
+        <img
+          src="https://ftp.goit.study/img/campers-test-task/1-1.webp"
+          className="object-cover w-full h-full object-center"
+          alt="camper 1"
+        />
+      </div>
+      <div className="w-full">
+        <div className="flex flex-row justify-between mb-2">
+          <h3 className="font-semibold text-[24px] leading-[1.33] text-dark">
+            Road Bear C 23-25
+          </h3>
+          <div className="flex gap-3 items-center">
+            <p className="font-semibold text-[24px] leading-[1.33] text-dark">
+              €10000
+            </p>
+            <Icon id="heart" width={26} height={24} />
+          </div>
+        </div>
+        <div className="flex items-center">
+          <Icon id="star-yellow" width={16} height={16} className="mr-1" />
+          <p className="mr-4">4.4(2 Reviews)</p>
+        </div>
+        <Button as="link" to="/catalog/1" width={166}>
+          Show more
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default CapmerCard;
