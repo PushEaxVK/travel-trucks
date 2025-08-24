1871d22c360f55fb7cf3021fd5871456db43d387
PushEax <pusheax.vk@gmail.com>
2025-08-24 04:20:54 +0300

    use camper data in CamperCard

--- src/components/CapmerCard.jsx
+++ src/components/CapmerCard.jsx
@@ -3,36 +3,42 @@ import Button from './Button';
 import Icon from './Icon';
 import FeatureBadge from './FeatureBadge';
 
-function CapmerCard() {
+function CapmerCard({ camperData }) {
+  // console.log(camperData);
+  const img = camperData.gallery[0].thumb;
+  const alt = `${camperData.name} Image`;
   return (
     <div className="p-6 flex flex-row gap-6 border border-grayLight rounded-[20px]">
       <div className="rounded-[10px] w-[292px] h-[320px] overflow-hidden shrink-0">
         <img
-          src="https://ftp.goit.study/img/campers-test-task/1-1.webp"
+          src={img}
           className="object-cover w-full h-full object-center"
-          alt="camper 1"
+          alt={alt}
         />
       </div>
       <div className="w-full">
         <div className="flex flex-row justify-between mb-2">
           <h3 className="font-semibold text-[24px] leading-[1.33] text-dark">
-            Road Bear C 23-25
+            {camperData.name}
           </h3>
           <div className="flex gap-3 items-center">
             <p className="font-semibold text-[24px] leading-[1.33] text-dark">
-              €10000
+              €{camperData.price}
             </p>
             <Icon id="heart" width={26} height={24} />
           </div>
         </div>
         <div className="flex items-center mb-6">
           <Icon id="star-yellow" width={16} height={16} className="mr-1" />
-          <p className="mr-4">4.4(2 Reviews)</p>
+          <p className="mr-4">
+            {camperData.rating}({camperData.reviews.length} Reviews)
+          </p>
           <Icon id="map" width={16} height={16} className="mr-1" />
-          <p>Kyiv, Ukraine</p>
+          <p>{camperData.location}</p>
         </div>
-        <p className="mb-6 font-normal text-base leading-6 text-grayDark">
-          The pictures shown here are example vehicles of the respective...
+        <p className="mb-6 font-normal text-base leading-6 text-grayDark line-clamp-1">
+          {/* The pictures shown here are example vehicles of the respective... */}
+          {camperData.description}
         </p>
         <div className="flex flex-wrap gap-2 mb-6">
           <FeatureBadge />
