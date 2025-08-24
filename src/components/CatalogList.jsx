2fa1d069400a8daa7289df7cec72c094a66354a4
PushEax <pusheax.vk@gmail.com>
2025-08-24 04:20:26 +0300

    get data of campers from redux

--- src/components/CatalogList.jsx
+++ src/components/CatalogList.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Button from './Button';
 import CapmerCard from './CapmerCard';
+import { useSelector } from 'react-redux';
+import { selectAllCampers } from '../redux/campers/selectors';
 
 function CatalogList() {
+  const items = useSelector(selectAllCampers);
   return (
     <div className="w-full flex items-center flex-col gap-10 pb-[52px]">
-      <div className="w-full flex flex-col gap-8">
-        <CapmerCard />
-        <CapmerCard />
-        <CapmerCard />
-        <CapmerCard />
-        <CapmerCard />
-      </div>
+      <ul className="w-full flex flex-col gap-8">
+        {items.map((item) => (
+          <li key={item.id}>
+            <CapmerCard camperData={item} />
+          </li>
+        ))}
+      </ul>
       <Button as="button" variant="white" onClick={() => {}} width={145}>
         Load more
       </Button>
