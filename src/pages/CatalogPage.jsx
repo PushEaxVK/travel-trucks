2befe5ac1b58c527908c69fd03b1a61b2754158c
PushEax <pusheax.vk@gmail.com>
2025-08-24 04:19:56 +0300

    fetch campers in useEffect on CatalogPage

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -2,15 +2,29 @@ import React, { useEffect } from 'react';
 import Container from '../components/Container';
 import SearchForm from '../components/SearchForm';
 import CatalogList from '../components/CatalogList';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllCampers } from '../redux/campers/operations';
+import { selectFilters } from '../redux/filters/selectors';
+import { selectPage } from '../redux/campers/selectors';
 
 function CatalogPage() {
   const dispatch = useDispatch();
+  const filters = useSelector(selectFilters);
+  const page = useSelector(selectPage);
+  console.log(filters);
+  console.log(page);
 
   useEffect(() => {
-    dispatch(fetchAllCampers({ page: 1 }));
-  }, [dispatch]);
+    dispatch(
+      fetchAllCampers({
+        page,
+        filterEquipment: filters.equipment,
+        filterVehicleType: filters.vechileType,
+        location: filters.location,
+      })
+    );
+  }, [dispatch, page, filters]);
+
   return (
     <Container>
       <div className="flex flex-row gap-16 pt-12">
